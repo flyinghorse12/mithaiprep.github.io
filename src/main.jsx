3c1cd7e1e715f29e904abf822e0dc29fdacee4e0
flyinghorse12 <80885110+flyinghorse12@users.noreply.github.com>
2025-10-27 16:33:01 +0530

    V2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { GoogleAuthProvider } from "./context/GoogleAuthProvider.jsx"; // ✅ import your provider
+import { GoogleAuthProvider } from "./context/GoogleAuthProvider.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -11,11 +10,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GoogleAuthProvider>  {/* ✅ Wrap the whole app */}
-        <App />
-      </GoogleAuthProvider>
-    </BrowserRouter>
+    <GoogleAuthProvider>
+      <App />
+    </GoogleAuthProvider>
   </React.StrictMode>
 );
 
